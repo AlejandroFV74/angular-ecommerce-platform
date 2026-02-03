bfa703799af2ad1f6c2d208cffaef1516d32ac36
AlejandroFV74 <00067323@uca.edu.sv>
2026-02-02 22:57:33 -0600

    Dynamict tasks manegment

--- ecommerceApp/src/app/pages/home/home.ts
+++ ecommerceApp/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,5 +8,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.css',
 })
 export class Home {
+  tasks = signal([
+    'Instalar Angular CLI',
+    'Crear un nuevo proyecto Angular',
+    'Configurar el entorno de desarrollo',
+  ]);
 
+  changeHandler(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const newTask = inputElement.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((tasks,position) => position !== index));
+
+}
 }
