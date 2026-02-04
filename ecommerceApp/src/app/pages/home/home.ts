ce527bb030e83784a6c0865688a869e079e0bd7b
AlejandroFV74 <00067323@uca.edu.sv>
2026-02-03 22:21:32 -0600

    Updated tasks with immutable states

--- ecommerceApp/src/app/pages/home/home.ts
+++ ecommerceApp/src/app/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,20 +10,45 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.css',
 })
 export class Home {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear un nuevo proyecto Angular',
-    'Configurar el entorno de desarrollo',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto Angular',
+      completed: false,
+    },
   ]);
 
   changeHandler(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     const newTask = inputElement.value;
+    this.addTask(newTask);
+  }
+
+  // Separar responsabilidad de agregar tarea
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title: title,
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   deleteTask(index: number) {
-    this.tasks.update((tasks) => tasks.filter((tasks,position) => position !== index));
+    this.tasks.update((tasks) => tasks.filter((tasks, position) => position !== index));
+  }
 
-}
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      });
+    });
+  }
 }
