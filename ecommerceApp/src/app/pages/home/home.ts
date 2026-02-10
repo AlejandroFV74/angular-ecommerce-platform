a16fdfd6b0c2df313195efbc48c51a6f8b47a189
AlejandroFV74 <00067323@uca.edu.sv>
2026-02-09 20:13:09 -0600

    Dynamic task editing implemented

--- ecommerceApp/src/app/pages/home/home.ts
+++ ecommerceApp/src/app/pages/home/home.ts
@@ -59,4 +59,41 @@ export class Home {
       });
     });
   }
+
+  updateTaskEditingMode(index: number) {
+    // Evitar entrar en modo edición si la tarea está completada
+    if(this.tasks()[index].completed){return}
+
+    this.tasks.update((prevState) => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true,
+          };
+        }
+        return {
+          ...task,
+          // Aseguramos que solo una tarea esté en modo edición a la vez
+          editing: false,
+        } 
+      });
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update((prevState) => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title : input.value,
+            editing: false, // Salir del modo edición después de actualizar el texto
+          };
+        }
+        return task;
+      });
+    });
+  }
 }
